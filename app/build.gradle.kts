e0bbca230084d9736e89fa62dde55e73e0c43a0e
Adrian Baluta <34679405+danadrianbaluta@users.noreply.github.com>
2026-07-22 22:19:57 +0100

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.resell.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "2.6"
+        versionCode = 18
+        versionName = "2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
